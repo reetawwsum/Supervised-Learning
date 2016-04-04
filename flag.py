3ef27ebb9a27a9032ad1121859ef00621b694857
Reet Awwsum <reetawwsum@yahoo.com>
2016-04-04 22:05:35 +0530

    Trying out first learning algorithm

--- flag.py
+++ flag.py
@@ -8,6 +8,9 @@ import csv
 import numpy as np
 from collections import Counter
 from sklearn import preprocessing
+from sklearn import cross_validation
+from sklearn import tree
+from sklearn import metrics
 from common.fn import *
 
 with open('datasets/flag.data', 'r') as input_file:
@@ -36,4 +39,16 @@ X[:, -1] = le.fit_transform(X[:, -1])
 X = X.astype(int)
 y = y.astype(int)
 
-# Visualise data
+sss = cross_validation.StratifiedShuffleSplit(y, 3, test_size=0.1, random_state=42)
+
+for train_index, test_index in sss:
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
+
+	clf = tree.DecisionTreeClassifier()
+	clf.fit(X_train, y_train)
+
+	y_predict = clf.predict(X_test)
+
+	print metrics.accuracy_score(y_predict, y_test)
+	# print metrics.classification_report(y_predict, y_test)
