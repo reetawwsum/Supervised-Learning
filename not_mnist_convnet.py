f9738def862b5a50baa2dccd97cce366c02df801
Reet Awwsum <reetawwsum@yahoo.com>
2016-05-01 13:22:29 +0530

    Fixing bug in tab space

--- not_mnist_convnet.py
+++ not_mnist_convnet.py
@@ -64,7 +64,7 @@ def conv2d(x, W):
 	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+  	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def inference(images):
 	# Conv1 Layer
